bc6d4f3d993beac1372853e795ed2efc7208f685
Laciface <csomali18@gmail.com>
2021-03-22 12:27:56 +0100

    add: install Laravel

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = [
+            0 => ['name' => 'Iphone', 'category' => 'smart phone', 'price' => 1000],
+            1 => ['name' => 'Galaxy', 'category' => 'tablet', 'price' => 2000],
+            2 => ['name' => 'Sony', 'category' => 'TV', 'price' => 3000],
+        ];
+        return view('allproducts', compact("$products"));
+    }
+}
