7a20cd11b1623e8d355767e9ff087e3fb1b05485
Laciface <csomali18@gmail.com>
2021-03-22 20:03:19 +0100

    refactor: template shows data from db

--- resources/views/allproducts.blade.php
+++ resources/views/allproducts.blade.php
@@ -269,7 +269,7 @@
                             <div class="product-image-wrapper">
                                 <div class="single-products">
                                     <div class="productinfo text-center">
-                                        <img src="{{ asset('images/home/product1.jpg')}}" alt="" />
+                                        <img src="{{Storage::disk('local')->url('images/' . $product->image)}}" alt="" />
                                         <h2>{{$product->price}}</h2>
                                         <p>{{$product->name}}</p>
                                         <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
