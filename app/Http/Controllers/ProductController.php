496cc5f4f88eaa89eb31a6dff22cf39c2b8b385b
Laciface <csomali18@gmail.com>
2021-03-22 16:20:33 +0100

    feature: list products with price

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -9,13 +10,28 @@ class ProductController extends Controller
 {
     public function index()
     {
+        /**
+         * example without database
+         */
         /*$products = [
             0 => ['name' => 'Iphone', 'category' => 'smart phone', 'price' => 1000],
             1 => ['name' => 'Galaxy', 'category' => 'tablet', 'price' => 2000],
             2 => ['name' => 'Sony', 'category' => 'TV', 'price' => 3000],
         ];*/
 
-        $products = DB::table('products')->get();
+        /**
+         * get data direct from database
+         *
+         */
+        /*$products = DB::table('products')->get();*/
+
+        /**
+         * get data indirect from database with model
+         *
+         */
+
+        $products = Product::all();
+
         return view('allproducts', compact("products"));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'type',
+        'price',
+    ];
+
+
+    public function getPriceAttribute($value)
+    {
+        return "$" . $value;
+    }
+
+
 }
