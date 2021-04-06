4abe7d020d324e9e71b8a81b8c5f8bca99906ba5
Laciface <csomali18@gmail.com>
2021-04-06 11:36:48 +0200

    feature: create addItem function

--- app/Cart.php
+++ app/Cart.php
@@ -4,7 +4,7 @@
 class Cart
 {
 
-    public $items;
+    public $items; //['id' =>['quantity' =>, 'price'=>, 'data'=> ]]
     public $totalQuantity;
     public $totalPrice;
     /**
@@ -21,5 +21,22 @@ class Cart
             $this->totalPrice = 0;
             $this->totalQuantity = 0;
         }
+    }
+
+    public function addItem($id, $product){
+        $price = (int) str_replace('$', '', $product->price);
+
+        //if item already exists
+        if(array_key_exists($id, $this->items)){
+            $productToAdd = $this->items[$id];
+            $productToAdd['quantity']++;
+        // first item occurrence
+        } else {
+            $productToAdd = ['quantity'=> 1, 'price'=> $price, 'data'=> $product];
+        }
+
+        $this->items[$id] = $productToAdd;
+        $this->totalQuantity++;
+        $this->totalPrice += $price;
     }
 }
